4f93801ff397f24e4c965c6bbd81b05a9d2dd64f
nickolsv <nickols1234@gmail.com>
2020-12-24 20:00:18 +0200

    Update basic app structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Header />
         <Navbar />
+
+        <Route exact={true} path="/">
+          <Home />
+        </Route>
+        
+        <Footer />
+      </Router>
     </div>
   );
 }
