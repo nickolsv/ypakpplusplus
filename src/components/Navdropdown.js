4c2d9b4827e2d2d2fb7b66b09cd6bb2d3756b6de
nickolsv <nickols1234@gmail.com>
2020-12-26 18:01:23 +0200

    Create navddcolumn element, change navdd props

--- src/components/Navdropdown.js
+++ src/components/Navdropdown.js
@@ -1,17 +1,34 @@
 import './Navdropdown.css'
 
+function NavdropdownColumn(props) {
+    
+    var title = null;
+    if( props.title != null )   title = (<div className="nav-col-title">{props.title}</div>)
+    
+    var renderArray = [];
+    props.links.forEach(link => {
+        renderArray.push(<a className="nav-col-element" href={link.href}>{link.title}</a>)
+    });
+
+    return(
+        <div>
+            {title}
+            {renderArray}
+        </div>
+    )
+}
+
 function Navdropdown(props) {
     
-    var resultArray = [];
+    var renderArray = [];
     props.options.forEach(
         element => {
-            var result = <div className="navdropdown-item">{element}</div>
-            resultArray.push(result);
+            renderArray.push(<NavdropdownColumn title={element.title} links={element.links}/>);
     });
     
     return(
         <div className="navdropdown">
-            {resultArray}
+            {renderArray}
         </div>
     );
 }
