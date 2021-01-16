cc14e4fac45ba39b7bdcc2cf760f1236b30b5476
nickolsv <nickols1234@gmail.com>
2021-01-16 19:21:12 +0200

    Add more routes in main app

--- src/App.js
+++ src/App.js
@@ -1,32 +1,44 @@
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-import Navbar from './components/Navbar';
 import Header from './components/Header';
-import Footer from './components/Footer';
 import Home from './components/Home';
+import Footer from './components/Footer';
+import {Banner, BannerLink} from './components/Banner';
+import Appointment from "./components/Appointment";
+import Login from "./components/Login";
+import {ReportLink, ReportPage} from './components/Report';
+import {ChoicesLink, ChoicesPage} from './components/Epiloges';
 import {Contact, Terms, About} from './components/FooterLinks';
-import Calendar from './components/ProfileLinks';
-import {Banner,BannerLink} from './components/Banner';
-
+import Register from "./components/Register";
 
 function App() {
+
+
   return (
     <div className="App">
       <Router>
         <Header />
-        <Navbar />
 
         <Route exact={true} path="/">
           <Home />
-          <Banner/>
+          <Banner />
         </Route>
-        
-        <Route path="/contact" component={Contact}/>
-        <Route path="/about" component={About}/>
-        <Route path="/terms" component={Terms}/>
-        <Route path="/calendar" component={Calendar}/>
-        <Route path="/covid19info" component={BannerLink}/>
+
+
+        <Route exact={true} path="/login">
+          <Login loginType="1" />
+        </Route>
+
+        <Route exact={true} path="/register" component={Register} />
+        <Route exact={true} path="/appointment" component={Appointment} />
+        <Route exact={true} path="/covid19info" component={BannerLink} />
+        <Route exact={true} path="/contact" component={Contact} />
+        <Route exact={true} path="/report" component={ReportPage}/>
+        <Route exact={true} path="/choices" component={ChoicesPage}/>
+        <Route exact={true} path="/terms" component={Terms}/>
+        <Route exact={true} path="/about" component={About}/>
+
         <Footer />
       </Router>
     </div>
