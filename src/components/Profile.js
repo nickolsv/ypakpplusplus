f6b1e33415966c8e2957021db66401e90b31a3dc
nickolsv <nickols1234@gmail.com>
2021-01-19 17:05:44 +0200

    Profile fix

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom';
 import './Profile.css';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faUserCircle} from '@fortawesome/free-solid-svg-icons';
+
 class Profile extends Component{
 
     state = {
@@ -13,30 +14,47 @@ class Profile extends Component{
     {
         var newState = Object.assign({},this.state);
         if(this.state.dropdownVisible)
-        {
             newState.dropdownVisible = false;
-        }
         else
-        {
             newState.dropdownVisible = true;
-        }
         this.setState(newState);
+    }
+
+    logout()
+    {
+        sessionStorage.clear();
+        window.location = "/";
     }
 
     render()
     {
-        var profile =(  <nav>
+        var isLoggedIn = this.props.isLoggedIn;
+        var profile = ( <nav>
+            <ul className="profile-links">
+                <Link to='/login' className="link">
+                    <li> Σύνδεση</li>
+                </Link>
+                <Link to='/register' className="link">
+                    <li> Εγγραφή</li>
+                </Link>
+            </ul>
+        </nav>)
+
+        if( isLoggedIn)
+            profile =(  <nav>
                             <ul className="profile-links">
                                 <Link to='/calendar' className="link">
                                     <li> Το Ημερολόγιό μου</li>
                                 </Link>
-                                <Link to='/' className="link">
+                                <Link onClick={() => this.logout()} to='/' className="link">
                                     <li>Αποσύνδεση</li>
                                 </Link>
                             </ul>
                         </nav>);
+
         return (
             <div className="profile" onClick={ () => this.dropdownDrop()}>
+                {sessionStorage.getItem("afm") ? <p className="user-name">{sessionStorage.fname + " " + sessionStorage.lname}</p> : null }
                 <FontAwesomeIcon icon={faUserCircle} className="user"/>
                 {this.state.dropdownVisible ? profile : null}
             </div>
